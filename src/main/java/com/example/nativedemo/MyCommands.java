9f4c99ec904971c0c47446f10e792e2418d9802c
Jiahao Yang <yangjiahao@csrd.cn>
2022-08-14 00:48:42 +0800

    init: add initial files

---
+++ src/main/java/com/example/nativedemo/MyCommands.java
@@ -0,0 +1,13 @@
+package com.example.nativedemo;
+
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+
+@ShellComponent
+public class MyCommands {
+
+    @ShellMethod("Add two integers together.")
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
---
+++ src/main/java/com/example/nativedemo/NativeDemoApplication.java
@@ -0,0 +1,21 @@
+package com.example.nativedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.shell.context.InteractionMode;
+import org.springframework.shell.context.ShellContext;
+
+@SpringBootApplication
+public class NativeDemoApplication {
+
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            args = new String[] {"help"};
+        }
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(NativeDemoApplication.class, args);
+        ShellContext shellContext = applicationContext.getBean(ShellContext.class);
+        shellContext.setInteractionMode(InteractionMode.NONINTERACTIVE);
+    }
+
+}
